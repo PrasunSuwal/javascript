bf3078a60f9f02cb30496479ff5d52f9a1154e64
Prasun Suwal <153846706+PrasunSuwal@users.noreply.github.com>
2024-11-23 17:18:12 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "Prasun"
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//NUMBER Conversion
+// "33"=>33
+// "33abc"=>NaN
+// true=>1, false=>0
+
+let isLoggedIn = "sfzsf"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//Boolean Conversion
+//1 => true, 0=>false
+//""=> false, "prasun"=>true
+
+let someNumber = 21
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

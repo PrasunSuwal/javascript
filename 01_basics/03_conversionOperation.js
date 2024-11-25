526931044d2d751bdaf2bc3cae3ffab05ff0c24a
Prasun Suwal <153846706+PrasunSuwal@users.noreply.github.com>
2024-11-25 15:20:30 +0000

    conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,19 @@ let someNumber = 21
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//***Operations *****
+
+let value = 3
+let negValue = -3
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Prasun"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+2+2);//in this it will be treated as string
+console.log(1+2+"3");//in this first add operation will take place 

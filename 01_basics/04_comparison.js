e09f03cd77182bd95f88472a54ea87c6bd7b40a1
Prasun Suwal <153846706+PrasunSuwal@users.noreply.github.com>
2024-11-25 15:35:08 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);// equality sign works differently than comparison signs
+// console.log(null >= 0);// this is true becoz comparisons convert null to a num i.e null is treated as 0
+
+console.log(undefined == 0);
+
+// === strict check, it checks values as well as data type 
+
+
+//we need to avoid all the above comparisons, it creates confusion

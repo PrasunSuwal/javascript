e84c3d7a7745c6d1f0d7a2da20aae51bc05e3d28
Prasun Suwal <153846706+PrasunSuwal@users.noreply.github.com>
2024-11-28 17:36:17 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+// arrays are resizeable i.e add can be added into the array
+
+//shallow and deep copy
+
+//shhalow copy means properties share same reference 
+//in array it creates shallow copy
+
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myArr2 = new Array(1, 2, 3, 4)//this another method of declaring array
+
+console.log(myArr[0]);
+
+//Array methods

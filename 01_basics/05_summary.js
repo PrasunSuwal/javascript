dbb74c0958d00c5b8153274a080cada818f5f0a6
Prasun Suwal <153846706+PrasunSuwal@users.noreply.github.com>
2024-11-28 16:13:06 +0000

    summary

---
+++ 01_basics/05_summary.js
@@ -0,0 +1,25 @@
+//number returntype number
+//symbol returntype symbol
+//boolean returntype boolean
+//string returntype string
+//bigInt returntype bigint
+//null returns object
+
+
+//array return object
+//object reurns object
+//function returns function which is call object function
+
+//With return: The function returns the value specified.
+//Without return: The function implicitly returns undefined.
+//Arrow Functions: Can implicitly return values in a concise syntax.
+//Functions as Values: Functions can return other functions.
+
+
+//-----------------------------------------------------
+
+//Memory: Stack and Heap
+
+//primitive type:stack, call by value
+//non-primitive:heap, call by  reference
+

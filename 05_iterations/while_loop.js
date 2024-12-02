d2e5c6fbb2025e5426ae1e28d9b821ed71631970
Prasun Suwal <153846706+PrasunSuwal@users.noreply.github.com>
2024-12-02 09:35:38 +0000

    loops

---
+++ 05_iterations/while_loop.js
@@ -0,0 +1,25 @@
+//while loop
+
+// let index = 0 // initialization
+// while (index <= 10) {//condition
+//     // console.log(`Value of index is ${index}`);
+//     index = index + 2;//incremntal cond
+// }
+
+// let myArray = ["ram","hari","shyam"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+
+//do while loop
+//first a work is done and then a condition is checked
+let score = 11
+do {
+   console.log(`Score is ${score}`);
+   score++;
+    
+} while (score <= 10);
